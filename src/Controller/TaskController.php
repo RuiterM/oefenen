adf7e8f427877dc3fa4e8f187e3d4b9e3c0a1e4b
RuiterM <302078305@student.rocmondriaan.nl>
2023-03-10 12:54:40 +0100

    forms added

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\TaskFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    #[Route('/task', name: 'app_task')]
+    public function index(): Response
+    {
+        return $this->render('task/index.html.twig', [
+            'controller_name' => 'TaskController',
+        ]);
+    }
+
+    #[Route('/taskform', name: 'app_task_form')]
+    public function index2(): Response
+    {
+        $task = new Task();
+        // ...
+
+        $form = $this->createForm(TaskFormType::class, $task);
+
+        return $this->renderForm('task/new.html.twig', [
+            'form' => $form,
+        ]);
+
+    }
+}
